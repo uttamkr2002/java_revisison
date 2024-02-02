1776e558f90b418d6cceac374d5f6cca52df7c0c
uttamkr2002 <sonuttam2002@gmail.com>
2024-02-02 22:39:33 +0530

    Succesfully run code for Recursive approach for Factorial

--- Functions/test1.java
+++ Functions/test1.java
@@ -5,13 +5,25 @@ public class test1 {
     // Find factorial
 
     public static int Factorial(int n){
-        int fact=1;
+         int fact=1; // Never Intialize with zero
 
        for(int i=n;i>1;i--){
           fact *= i;
        }
        return fact;
 
+    }
+
+    // Factorial Using Recursive Approach
+
+    public static int Rec_Fact(int n){
+
+        // base case
+        if(n<=2){
+            return n;
+        }
+
+        return n*Rec_Fact(n-1);
     }
 
     // Find Bionomial Coefficient
@@ -26,6 +38,7 @@ public class test1 {
 
     public static void main(String args[]){
       System.out.println(  Factorial(5));
+      System.out.println(Rec_Fact(5));
     }
 
 }
